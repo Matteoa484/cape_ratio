d4531e040a843079e4e01b6d659ee5d8943cb188
Matteo Anrò <ma@sumuscapital.com>
2021-03-08 14:57:25 +0100

    correction to returns code

--- R/eda.R
+++ R/eda.R
@@ -30,20 +30,32 @@ sp_xts <-
     dplyr::select(date, price) %>%
     xts::xts(x = .[, -1], order.by = .[[1]])
 
-
+# S&P 500 total returns
 sp_ret <-
     xts::merge.xts(
-        sp_xts / xts::lag.xts(sp_xts, k = 6) - 1,
-        sp_xts / xts::lag.xts(sp_xts, k = 12) - 1,
-        sp_xts / xts::lag.xts(sp_xts, k = 36) - 1,
-        sp_xts / xts::lag.xts(sp_xts, k = 60) - 1,
-        sp_xts / xts::lag.xts(sp_xts, k = 120) - 1
+        xts::lag.xts(sp_xts, k = -6)   / sp_xts - 1,
+        xts::lag.xts(sp_xts, k = -12)  / sp_xts - 1,
+        xts::lag.xts(sp_xts, k = -36)  / sp_xts - 1,
+        xts::lag.xts(sp_xts, k = -60)  / sp_xts - 1,
+        xts::lag.xts(sp_xts, k = -120) / sp_xts - 1
     ) %>%
     `colnames<-`(c('6_m', '1_y', '3_y', '5_y', '10_y'))
 
 
-    
-  
+
+test_dt <- seq.Date(Sys.Date() - 20, Sys.Date(), by = 'days')
+test_vl <- seq(1, 21, by = 1)
+
+test_xts <- 
+    xts::xts(x = test_vl, order.by = test_dt) %>%
+    `colnames<-`('base_data')
+
+test_xts$lag_1 <- xts::lag.xts(test_xts$base_data, k = 1)
+test_xts$lag_6 <- xts::lag.xts(test_xts$base_data, k = 6)
+test_xts$lag_new <- xts::lag.xts(test_xts$base_data, k = -6)
+
+
+
 x <- sp_ret$`6_m` %>% na.omit %>% .[1, ]
 y <- sp_ret$`1_y` %>% na.omit %>% .[1, ]
 z <- sp_ret$`5_y` %>% na.omit %>% .[1, ]
@@ -83,6 +95,20 @@ xts::merge.xts(cape_xts, ann_ret_5y, join = 'inner') %>%
     dplyr::mutate(date = zoo::as.yearmon(date)) %>%
     `colnames<-`(c('date', 'cape', '5y_ret')) %>%
     ggplot(aes(x = cape, y = `5y_ret`)) +
+    geom_point() +
+    geom_smooth(method = 'lm', se = FALSE) +
+    scale_y_continuous(labels = scales::percent_format(accuracy = 1))
+
+
+ann_ret_10y <- ((1 + sp_ret$`10_y`) ^ (12 / 120)) - 1
+
+# test plot
+xts::merge.xts(cape_xts, ann_ret_10y, join = 'inner') %>%
+    na.omit() %>%
+    tibble::as_tibble(rownames = 'date') %>%
+    dplyr::mutate(date = zoo::as.yearmon(date)) %>%
+    `colnames<-`(c('date', 'cape', '10y_ret')) %>%
+    ggplot(aes(x = cape, y = `10y_ret`)) +
     geom_point() +
     geom_smooth(method = 'lm', se = FALSE) +
     scale_y_continuous(labels = scales::percent_format(accuracy = 1))
